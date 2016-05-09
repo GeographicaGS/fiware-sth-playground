1226d7e6c20814ed661a617b68bdd61af9fb4529
cayetanobv <cayetanobv@gmail.com>
2016-05-09 02:53:16 +0200

    updated scripts

--- fiware-orion/scripts/contextbroker_subscription_sth.sh
+++ fiware-orion/scripts/contextbroker_subscription_sth.sh
@@ -10,7 +10,7 @@ curl localhost:1026/v1/subscribeContext -s -S --header 'Content-Type: applicatio
     "attributes": [
             "s","f"
     ],
-    "reference": "http://fiware-sth-comet:8666/notify",
+    "reference": "http://sthcomet:8666/notify",
     "duration": "P1M",
     "notifyConditions": [
         {
@@ -22,5 +22,3 @@ curl localhost:1026/v1/subscribeContext -s -S --header 'Content-Type: applicatio
     ],
     "throttling": "PT5S"
 }'
-
-curl orion.contextBroker.host:1026/v1/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: theService' --header 'Fiware-ServicePath: theServicePath' -d
